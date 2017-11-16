d96baf16953564dcc5fce08c4b4056b2ec6fd1e1
Luke Schlangen <lukeschlangen@gmail.com>
2017-11-16 16:22:09 -0600

    removing index.js and replacing with express.static

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-var index = require('./routes/index');
 var hello = require('./routes/welcome');
 var parts = require('./routes/parts');
 var bodyParser = require('body-parser');
@@ -9,13 +8,11 @@ var port = 8000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', index);
+app.use(express.static('server/public'));
 
 app.get('/hello', parts); // Oh, hi there! Wait, who's parts are these?
 
 app.use('/parts', parts);
-
-app.use(express.static('server/public'));
 
 app.listen(port);
 console.log("Listening on port: ", port);
