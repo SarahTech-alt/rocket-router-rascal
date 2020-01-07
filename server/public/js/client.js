13911011a4fb6c5c4799440b202d0eaed90d3fa9
Dane Smith <doctorhowser@gmail.com>
2020-01-07 07:47:18 -0600

    update for modern code

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-  var spaceshipParts = [];
+  const spaceshipParts = [];
 
   $.ajax({
     type: 'GET',
-    url: '/hello',
-    success: function(data){
-      $('h1').text(data);
-    },
-    error: function(error){
-      console.log('The "/hello" ajax get request failed with error: ', error);
-    }
+    url: '/hello'
+  }).then(function(data){
+    $('h1').text(data);
+  }).catch(function(error){
+    console.log('The "/hello" ajax get request failed with error: ', error);
   });
 
   getParts();
@@ -17,21 +15,20 @@ $(document).ready(function(){
   function getParts(){
     $.ajax({
       type: 'GET',
-      url: '/parts',
-      success: function(partsData){
-        displayParts(partsData);
-      },
-      error: function(error){
-        console.log('The "/parts" ajax get request failed with error: ', error);
-      }
-    });
+      url: '/parts'
+    }).then(function(partsData){
+      displayParts(partsData);
+    }).catch(function(error){
+      console.log('The "/parts" ajax get request failed with error: ', error);
+    })
+      
     getRocketPossibleCount();
   }
 
   function displayParts(partThings){
     $('tbody').empty();
-    for(var i = 0; i < partThings.length; i++){
-      var newRow = $('<tr>');
+    for(let i = 0; i < partThings.length; i++){
+      let newRow = $('<tr>');
       newRow.data('partId', partThings[i].id);
       newRow.append('<td>' + partThings[i].name + '</td>');
       newRow.append('<td>' + partThings[i].needed + '</td>');
@@ -43,8 +40,8 @@ $(document).ready(function(){
   // New part
   $('#newPartInfo').on('submit', function(event) {
     event.preventDefault();
-    var newPartObject = {};
-    var fields = $('#newPartInfo').serializeArray();
+    let newPartObject = {};
+    let fields = $('#newPartInfo').serializeArray();
     fields.forEach(function(element, index, array) {
       newPartObject[element.name] = element.value;
     });
@@ -58,15 +55,14 @@ $(document).ready(function(){
     $.ajax({
       type: 'POST',
       url: '/parts/new',
-      data: newestPart,
-      success: function(response){
-        console.log(response);
-        getParts();
-      },
-      error: function(error){
-        console.log('The "/part" ajax post request failed with error: ', error);
-      }
-    });
+      data: newestPart
+    }).then(function(response){
+      console.log(response);
+      getParts();
+    }).catch(function(error){
+      console.log('The "/part" ajax post request failed with error: ', error);
+    })
+    
   }
 
   // Calculate number of rockets you can build
@@ -74,14 +70,12 @@ $(document).ready(function(){
     $.ajax({
       type: 'GET',
       url: '/parts/rocketCount',
-      success: function(data){
-        console.log(data);
-        $('#numberOfSpaceships').text(data.count);
-      },
-      error: function(error){
-        console.log('The "/rocketCount" ajax get request failed with error: ', error);
-      }
-    });
+    }).then(function(data){
+      console.log(data);
+      $('#numberOfSpaceships').text(data.count);
+    }).catch(function(error){
+      console.log('The "/rocketCount" ajax get request failed with error: ', error);
+    })
   }
 
 });
